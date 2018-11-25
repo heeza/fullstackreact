0aa96044210562b75b4c42fa50df58a53f8ea34f
HEEZA\heeza <heeza@ymail.com>
2018-11-25 23:09:15 +0900

    re commit

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,4 @@
 // keys.js - figure out what set of credentials to return
-
-// prod
-// 450223430630-fejehivorkv8bs07u3sn9pdk8kvkdbv9.apps.googleusercontent.com
-// I2Sh0lVatYfcum7InDB7wTfE
-
 if (process.env.NODE_ENV == 'production') {
     // we are in production - return the prod set of keys
     module.exports = require('./prod');
